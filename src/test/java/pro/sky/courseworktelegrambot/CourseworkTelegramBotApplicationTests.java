2eb50484417f960144696e83b5b630702493c0ab
Anton <Soloviev.gvardiya@gmail.com>
2023-08-27 16:22:06 +0300

    Initial commit

---
+++ src/test/java/pro/sky/courseworktelegrambot/CourseworkTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.courseworktelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseworkTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
