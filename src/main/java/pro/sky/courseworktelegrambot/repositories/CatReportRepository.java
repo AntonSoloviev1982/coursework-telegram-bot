af1c01cf41253b41a0f96084caa6bb45663dc3f4
AlexanderElkin1969 <eaa@mfcmmr.ru>
2023-09-09 00:13:47 +0300

    CatReport add Repository

---
+++ src/main/java/pro/sky/courseworktelegrambot/repositories/CatReportRepository.java
@@ -0,0 +1,15 @@
+package pro.sky.courseworktelegrambot.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pro.sky.courseworktelegrambot.entity.CatReport;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface CatReportRepository extends JpaRepository<CatReport, Long> {
+
+    List<CatReport> findAllById(Long id);
+
+    List<CatReport> findAllByReportDate(LocalDate localDate);
+
+}
