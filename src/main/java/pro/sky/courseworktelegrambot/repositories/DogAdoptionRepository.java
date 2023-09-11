65b728a0c2036f29d7fbe4a30439dfcac6a8966f
kodarov_salavat <kodarov@gmail.com>
2023-09-11 22:51:44 +0400

    fix DogReportRepository

--- src/main/java/pro/sky/courseworktelegrambot/repositories/DogAdoptionRepository.java
+++ src/main/java/pro/sky/courseworktelegrambot/repositories/DogAdoptionRepository.java
@@ -3,6 +3,10 @@ package pro.sky.courseworktelegrambot.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pro.sky.courseworktelegrambot.entities.DogAdoption;
+
+import java.util.List;
+
 @Repository
 public interface DogAdoptionRepository extends JpaRepository<DogAdoption,Integer> {
+    boolean existsByUserIdOrPetId(Integer userId, Integer petId);
 }
